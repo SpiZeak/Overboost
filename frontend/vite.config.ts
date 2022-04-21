92cd36d7de41cfb004391a9d598ab2c0a9e000ea
Max Trewhitt <max.trewhitt@osolo.se>
2022-04-22 01:08:42 +0200

    Add: PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        'images/favicon.svg',
+      ],
+      manifest: {
+        name: 'Overboost',
+        short_name: 'Overboost',
+        description: 'Web tuning app',
+        theme_color: '#ffffff',
+      }
+    }),
+    react()
+  ],
   server: {
+    https: true,
     watch: {
       usePolling: true
     }
